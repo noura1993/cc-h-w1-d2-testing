c8f1848095cb5f2c9fa7449b45ca378bc7f3c8c0
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-05-13 01:10:25 +0100

    Add some tests

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -7,3 +7,31 @@ end
 def return_10()
     return 10
 end
+
+def add(num_1, num_2)
+    return num_1 + num_2
+end
+
+def subtract(num_1, num_2)
+    return num_1 - num_2
+end
+
+def multiply(num_1, num_2)
+    return num_1 * num_2
+end
+
+def divide(num_1, num_2)
+    return num_1 / num_2
+end
+
+def length_of_string(str)
+    return str.length()
+end
+
+def join_string(str_1, str_2)
+    return str_1 + str_2
+end
+
+def add_string_as_number(str_1, str_2)
+    return str_1.to_i() + str_2.to_i() 
+end
