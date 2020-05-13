3f3fd0bc01d80eda883ef1e0a8aa3967cbb52c00
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-05-13 01:24:16 +0100

    Add more tests

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -35,3 +35,25 @@ end
 def add_string_as_number(str_1, str_2)
     return str_1.to_i() + str_2.to_i() 
 end
+
+def number_to_full_month_name(month_number)
+    case month_number
+        when 1
+            return "January"
+        when 3
+            return "March"
+        when 9
+            return "September"
+    end
+end
+
+def number_to_short_month_name(month_number)
+    case month_number
+        when 1
+            return "Jan"
+        when 4
+            return "Apr"
+        when 10
+            return "Oct"
+    end
+end
